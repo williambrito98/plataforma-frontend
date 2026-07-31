4a8cc93198cfeb1d236c200f1bdb2b9d748c0ec8
williambrito1998 <williambrito199850@gmail.com>
2026-07-31 14:41:49 -0300

    fix: css class

--- src/features/admin/components/admin-sidebar.tsx
+++ src/features/admin/components/admin-sidebar.tsx
@@ -14,13 +14,13 @@ import { AdminIcon } from "@/features/admin/components/admin-icon";
 export function AdminSidebar() {
   return (
     <Sidebar collapsible="icon" className="border-r-0">
-      <SidebarHeader className="px-6 pt-[38px] pb-0 group-data-[collapsible=icon]:flex group-data-[collapsible=icon]:justify-center group-data-[collapsible=icon]:px-3.5 group-data-[collapsible=icon]:pt-[38px]">
-        <div className="flex h-9 w-[43px] items-center justify-center group-data-[collapsible=icon]:w-auto">
-          <AdminIcon name="logo" size={36} className="h-9 w-[43px]" />
+      <SidebarHeader className="px-6 pt-9.5 pb-0 group-data-[collapsible=icon]:flex group-data-[collapsible=icon]:justify-center group-data-[collapsible=icon]:px-3.5 group-data-[collapsible=icon]:pt-9.5">
+        <div className="flex h-9 w-10.75 items-center justify-center group-data-[collapsible=icon]:w-auto">
+          <AdminIcon name="logo" size={36} className="h-9 w-10.75" />
         </div>
       </SidebarHeader>
 
-      <SidebarContent className="flex flex-col justify-between gap-[38px] px-6 pt-[38px] pb-6 group-data-[collapsible=icon]:items-center group-data-[collapsible=icon]:px-3.5">
+      <SidebarContent className="flex flex-col justify-between gap-9.5 px-6 pt-9.5 pb-6 group-data-[collapsible=icon]:items-center group-data-[collapsible=icon]:px-3.5">
         <AdminSidebarNav />
         <AdminSidebarDarkModeItem />
       </SidebarContent>
